cf29377bb9a7b4435d98e8b1192cb55bde758d07
Mark Trevathan <marktrevathan@rocketmail.com>
2025-01-15 23:18:59 -0500

    Fixed some issues in the code. Removed for loops.

--- app.js
+++ app.js
@@ -5,37 +5,14 @@ form.addEventListener('submit', (e) => {
   e.preventDefault();
   const text = input.value;
   input.value = '';
-
-  const paragraphLength = 500;
-
   const ul = document.getElementById('invitedList');
   const li = document.createElement('li');
-  
   li.textContent = text;
-  
   const label = document.createElement('label');
   label.textContent = 'Confirmed';
-
   const checkbox = document.createElement('input');
   checkbox.type = 'checkbox';
-
   label.appendChild(checkbox);
   li.appendChild(label);
   ul.appendChild(li);
-
- 
-  for (i = 1; i<=paragraphLength; i++) {
-    console.log(i);
-    console.log(paragraphLength);
-    console.log(ul);
-    console.log(li);
-    num = 5;
-    
-    li.appendChild(label);
-
-    ul.appendChild(li);
-
-  }
-
-  console.log(num);
 });
