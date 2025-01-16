b4aec751baf5f96277a0c620a16431d024486d75
Steven Trevathan <steven@makemodel.co>
2025-01-15 17:54:16 -0800

    Update app.js

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ form.addEventListener('submit', (e) => {
   e.preventDefault();
   const text = input.value;
   input.value = '';
+  console.log('text');
+  
   const ul = document.getElementById('invitedList');
   const li = document.createElement('li');
   li.textContent = text;
